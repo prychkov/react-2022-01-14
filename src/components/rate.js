6ef671769c791fda63f94e2d66ec2de7ff59449e
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-01-22 16:35:17 +0600

    changed rate component: changed cycle 'for' to 'map'

--- src/components/rate.js
+++ src/components/rate.js
@@ -4,16 +4,13 @@ import { ReactComponent as StarFilled } from '../icons/starFilled.svg';
 import { ReactComponent as StarNotFilled } from '../icons/starNotFilled.svg';
 
 export default function Rate({value}) {
-  let starsFilled = [];
-  let starNotFilled = [];
+  const starsFilled = [...Array(value)].map((_, i) => (
+    <StarFilled key={i} className = {styles.filled}/>
+  ));
 
-  for(let i = 0; i < value; i++) {
-    starsFilled.push(<StarFilled key={i} className = {styles.filled}/>);
-  }
-
-  for(let i = 0; i < (5 - value); i++) {
-    starNotFilled.push(<StarNotFilled key={i} className = {styles.notFilled}/>);
-  }
+  const starNotFilled = [...Array(5 - value)].map((_, i) => (
+    <StarNotFilled key={i} className = {styles.notFilled}/>
+  ));
 
   return (
     <div>
