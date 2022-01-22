cd3575028f21a17aff46ab7fb07c4f785d95e6d3
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-01-22 16:53:53 +0600

    change restaurant component: put calculation in usememo hook

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,18 +1,21 @@
+import { useMemo } from 'react';
 import Menu from './menu';
 import Reviews from './reviews';
 import Rate from './rate';
 
 export default function Restaurant({menu, reviews}) {
-  const rating =  reviews.map(review => review.rating);
-  const averageRating = Math.round( rating.reduce((sum, current) => sum + current) / reviews.length );
+
+  const averageRating = useMemo(()=> {
+    const rating =  reviews.map(review => review.rating);
+    return Math.round( rating.reduce((sum, current) => sum + current) / reviews.length );
+  }, [reviews]);
 
   return (
     <div>
       <Menu menu={menu}/>
       <Reviews reviews={reviews}/>
-      <p> Средний рейтинг по рестарану:</p>
+      <p> Average rating for a restaurant:</p>
       <Rate value={averageRating}/>
     </div>
-    
   );
 }
