83a50c4a82cd8cece0705b638d71c2b40d047dc0
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-02-20 22:44:46 +0600

    changes reducer users.js added constats FAILURE, LOAD_USERS, REQUEST, SUCCESS; added initialState; changed draft = initialState; added data, error in const; added case LOAD_USERS + REQUEST; added case LOAD_USERS + SUCCESS; added case LOAD_USERS + FAILURE;

--- src/redux/reducer/users.js
+++ src/redux/reducer/users.js
@@ -1,12 +1,40 @@
 import produce from 'immer';
+import { FAILURE, LOAD_USERS, REQUEST, SUCCESS } from '../constants';
 import { ADD_REVIEW } from '../constants';
-import { normalizedUsers } from '../../fixtures';
+//import { normalizedUsers } from '../../fixtures';
 import { arrToMap } from '../utils';
 
-export default produce((draft = arrToMap(normalizedUsers), action) => {
-  const { type, review, userId } = action;
+const initialState = {
+  entities: {},
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+export default produce((draft = initialState, action) => {
+  const { type, data, error, review, userId } = action;
 
   switch (type) {
+    case LOAD_USERS + REQUEST:
+      return {
+        ...draft,
+        loading: true,
+        error: null,
+      };
+    case LOAD_USERS + SUCCESS:
+      return {
+        ...draft,
+        entities: arrToMap(data),
+        loading: false,
+        loaded: true,
+      };
+    case LOAD_USERS + FAILURE:
+      return {
+        ...draft,
+        loading: false,
+        loaded: false,
+        error,
+      };
     case ADD_REVIEW:
       const { name } = review;
       draft[userId] = { id: userId, name };
