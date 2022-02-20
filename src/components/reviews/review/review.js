ef0903caa9e7f9c05ad1ff5442d88cc852a24354
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-02-20 23:26:33 +0600

    review.js: >> added fields loading, loaded to mapStateToProps; >> added mapDispatchToProps; >> added useEffect; >> added Loader; >> added check to const Review.

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -1,28 +1,39 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import Loader from '../../loader';
 import Rate from '../../rate';
 import styles from './review.module.css';
 
-import { reviewWitUserSelector } from '../../../redux/selectors';
+import { reviewWitUserSelector, usersLoadingSelector, usersLoadedSelector } from '../../../redux/selectors';
+import { loadUsers } from '../../../redux/actions';
 
-const Review = ({ user, text, rating }) => (
-  <div className={styles.review} data-id="review">
-    <div className={styles.content}>
-      <div>
-        <h4 className={styles.name} data-id="review-user">
-          {user}
-        </h4>
-        <p className={styles.comment} data-id="review-text">
-          {text}
-        </p>
-      </div>
-      <div className={styles.rate}>
-        <Rate value={rating} />
+const Review = ({ user, text, rating, loading, loaded, loadUsers }) => {
+  useEffect(() => {
+    if(!loading && !loaded) loadUsers();
+  }, [loading, loaded, loadUsers]);
+
+  if(loading) return <Loader />;
+  if(!loaded) return 'No data :(';
+  return (
+    <div className={styles.review} data-id="review">
+      <div className={styles.content}>
+        <div>
+          <h4 className={styles.name} data-id="review-user">
+            {user}
+          </h4>
+          <p className={styles.comment} data-id="review-text">
+            {text}
+          </p>
+        </div>
+        <div className={styles.rate}>
+          <Rate value={rating} />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Review.propTypes = {
   user: PropTypes.string,
@@ -40,6 +51,16 @@ Review.defaultProps = {
 
 // const mapStateToProps = (state, props) => reviewWitUserSelector(state, props);
 
-const mapStateToProps = reviewWitUserSelector;
+//const mapStateToProps = reviewWitUserSelector;
 
-export default connect(mapStateToProps)(Review);
+const mapStateToProps = (state, props) => ({
+  ...reviewWitUserSelector(state, props),
+  loading: usersLoadingSelector(state, props),
+  loaded: usersLoadedSelector(state, props),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    loadUsers: () => dispatch(loadUsers()),
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Review);
