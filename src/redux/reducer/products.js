76ca55230a3a7c37e2051e27d732b47ae788be7b
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-02-28 14:52:58 +0600

    added immer to products.js reducer

--- src/redux/reducer/products.js
+++ src/redux/reducer/products.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {FAILURE, LOAD_PRODUCTS, REQUEST, SUCCESS} from '../constants';
 import { arrToMap } from '../utils';
 
@@ -8,31 +9,28 @@ const initialState = {
   error: null,
 }
 
-export default (state = initialState, action) => {
+export default (state = initialState, action) => 
+produce(state, (draft) => {
   const { type, restId, data, error } = action;
   switch (type) {
-    case LOAD_PRODUCTS + REQUEST:
-      return {
-        ...state,
-        loading: {[restId]: true},
-        //error: null,
-      };
-    case LOAD_PRODUCTS + SUCCESS:
-      const {entities, loaded} = state;
-      return {
-        ...state,
-        loading: {[restId]: false},
-        loaded: {...loaded, [restId]: true},
-        entities: Object.assign(entities, arrToMap(data)),
-      };
-    case LOAD_PRODUCTS + FAILURE:
-      return {
-        ...state,
-        loading: {[restId]: false},
-        loaded: {[restId]: false},
-        error,
-      };
+    case LOAD_PRODUCTS + REQUEST: {
+      draft.loading[restId] = true;
+      break;
+    }
+    case LOAD_PRODUCTS + SUCCESS: {
+      draft.loading[restId] = false;
+      draft.loaded[restId] = true;
+      draft.error = null;
+      Object.assign(draft.entities, arrToMap(data));
+      break;
+    }
+    case LOAD_PRODUCTS + FAILURE: {
+        draft.loading[restId] = false;
+        draft.loaded[restId] = false;
+        draft.error = error;
+        break;
+    }
     default:
-      return state;
+      return;
   }
-};
+});
