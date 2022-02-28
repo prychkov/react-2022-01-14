7d1097e1941b7c1bdb0c099a85d0c30dd07b0715
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-02-28 14:39:22 +0600

    added immer to reviews.js

--- src/redux/reducer/reviews.js
+++ src/redux/reducer/reviews.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import { LOAD_REVIEWS, REQUEST, SUCCESS, FAILURE, ADD_REVIEW } from '../constants';
 import { arrToMap } from '../utils';
 
@@ -8,39 +9,32 @@ const initialState = {
   error: null,
 }
 
-export default (state = initialState, action) => {
+export default produce((draft = initialState, action) => {
   const { type, restId, data, error, review, reviewId, userId } = action;
-  const {entities} = state;
-  
+
   switch (type) {
-    case LOAD_REVIEWS + REQUEST:
-      return {
-        ...state,
-        loading: {[restId]: true },
-      }
-    case LOAD_REVIEWS + SUCCESS:
-      const {loaded} = state;
-      return {
-        ...state,
-        loading: {[restId]: false},
-        loaded: {...loaded, [restId]: true},
-        entities: Object.assign(entities, arrToMap(data)),
-        //entities: {...entities, ...arrToMap(data)}, // то же самое что строчка выше
-      }
-    case LOAD_REVIEWS + FAILURE:
-      return {
-        ...state,
-        loading: {[restId]: false},
-        loaded: {[restId]: false},
-        error: error,
-      }
+    case LOAD_REVIEWS + REQUEST: {
+      draft.loading[restId] = true;
+        draft.error = null;
+        break;
+    }
+    case LOAD_REVIEWS + SUCCESS: {
+        draft.loading[restId] = false;
+        draft.loaded[restId] = true;
+        Object.assign(draft.entities, arrToMap(data));
+        break;
+    }
+    case LOAD_REVIEWS + FAILURE: {
+        draft.loading[restId] = false;
+        draft.loaded[restId] = false;
+        draft.error = error;
+        break;
+    }
     case ADD_REVIEW:
       const { text, rating } = review;
-      return {
-        ...state,
-        entities: {...entities, [reviewId]: { id: reviewId, userId, text, rating }},
-      };
+      draft.entities[reviewId] = { id: reviewId, userId, text, rating };
+        break;
     default:      
-      return state;
+      return draft;
   }
-};
+});
