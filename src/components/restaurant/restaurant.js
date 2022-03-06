2dd653beeb655871d8f2dbe0c03de6f378dafda8
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-03-06 17:33:04 +0600

    ht6: added restautant.module.css to src/components/restaurant; added to restaurant component Switch,  Route and Redirect:

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+/* import { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Menu from '../menu';
@@ -29,6 +29,97 @@ const Restaurant = ({ restaurant, averageRating }) => {
       <Tabs tabs={tabs} activeId={activeTab} onChange={setActiveTab} />
       {activeTab === 'menu' && <Menu menu={menu} key={id} restId={id} />}
       {activeTab === 'reviews' && <Reviews reviews={reviews} restId={id} />}
+    </div>
+  );
+};
+
+Restaurant.propTypes = {
+  restaurant: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    menu: PropTypes.array,
+    reviews: PropTypes.array,
+  }).isRequired,
+  averageRating: PropTypes.number,
+};
+
+const mapStateToProps = (state, props) => ({
+  restaurant: restaurantSelector(state, props),
+  averageRating: averageRatingSelector(state, props),
+});
+
+export default connect(mapStateToProps)(Restaurant);
+ */
+
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import {NavLink, Switch, Route, Redirect, Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Menu from '../menu';
+import Reviews from '../reviews';
+import Banner from '../banner';
+import Rate from '../rate';
+import {
+  averageRatingSelector,
+  restaurantSelector,
+} from '../../redux/selectors';
+
+import cn from 'classnames';
+
+import styles from './restautant.module.css';
+
+const Restaurant = ({ restaurant, averageRating }) => {
+  const { id, name, menu, reviews } = restaurant;
+
+  const [activeTab, setActiveTab] = useState('menu');
+
+  const tabs = [
+    { id: 'menu', label: 'Menu' },
+    { id: 'reviews', label: 'Reviews' },
+  ];
+
+  return (
+    <div>
+      <Banner heading={name}>
+        <Rate value={averageRating} />
+      </Banner>
+      
+      {/* <div className={styles.tabs}>
+        {tabs.map(({ id, label }) => (
+          <span
+            key={id}
+            className={cn(styles.tab, { [styles.active]: id === activeTab })}
+            onClick={() => setActiveTab(id)}
+          >
+            {label}
+          </span>
+        ))}
+      </div> */}
+
+      {/* {() => activeTab === 'menu' && <Menu menu={menu} key={id} restId={id} />}
+      {() => activeTab === 'reviews' && <Reviews reviews={reviews} restId={id} />} */}
+
+      {<div className={styles.tabs}>
+        {tabs.map(({ id, label }) => (
+          <span
+            key={id}
+            className={cn(styles.tab, { [styles.active]: id === activeTab })}
+            onClick={() => setActiveTab(id)}
+          >
+            {label}
+          </span>
+        ))}
+      </div>}
+
+      <Switch>
+        <Route path="/restaurants/:restId/menu">
+          {() => <Menu menu={menu} key={id} restId={id} />}
+        </Route>
+        <Route path="/restaurants/:restId/reviews">
+          {() => <Reviews reviews={reviews} restId={id} />}
+        </Route>
+        <Redirect to={`/restaurants/${id}/menu`}/>
+      </Switch>
     </div>
   );
 };
