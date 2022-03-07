1483bbc9f420a8bffa5c63f4b9acfafee4efcbc6
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-03-07 17:55:00 +0600

    deleted unnecessary from restaurant.js

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,59 +1,5 @@
-/* import { useState } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import Menu from '../menu';
-import Reviews from '../reviews';
-import Banner from '../banner';
-import Rate from '../rate';
-import Tabs from '../tabs';
-import {
-  averageRatingSelector,
-  restaurantSelector,
-} from '../../redux/selectors';
-
-const Restaurant = ({ restaurant, averageRating }) => {
-  const { id, name, menu, reviews } = restaurant;
-
-  const [activeTab, setActiveTab] = useState('menu');
-
-  const tabs = [
-    { id: 'menu', label: 'Menu' },
-    { id: 'reviews', label: 'Reviews' },
-  ];
-
-  return (
-    <div>
-      <Banner heading={name}>
-        <Rate value={averageRating} />
-      </Banner>
-      <Tabs tabs={tabs} activeId={activeTab} onChange={setActiveTab} />
-      {activeTab === 'menu' && <Menu menu={menu} key={id} restId={id} />}
-      {activeTab === 'reviews' && <Reviews reviews={reviews} restId={id} />}
-    </div>
-  );
-};
-
-Restaurant.propTypes = {
-  restaurant: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string,
-    menu: PropTypes.array,
-    reviews: PropTypes.array,
-  }).isRequired,
-  averageRating: PropTypes.number,
-};
-
-const mapStateToProps = (state, props) => ({
-  restaurant: restaurantSelector(state, props),
-  averageRating: averageRatingSelector(state, props),
-});
-
-export default connect(mapStateToProps)(Restaurant);
- */
-
-import { useState } from 'react';
-import { connect } from 'react-redux';
-import {NavLink, Switch, Route, Redirect, Link} from 'react-router-dom';
+import {NavLink, Switch, Route, Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Menu from '../menu';
 import Reviews from '../reviews';
@@ -63,15 +9,11 @@ import {
   averageRatingSelector,
   restaurantSelector,
 } from '../../redux/selectors';
-
-import cn from 'classnames';
 
 import styles from './restautant.module.css';
 
 const Restaurant = ({ restaurant, averageRating }) => {
   const { id, name, menu, reviews } = restaurant;
-
-  const [activeTab, setActiveTab] = useState('menu');
 
   const tabs = [
     { id: 'menu', label: 'Menu' },
